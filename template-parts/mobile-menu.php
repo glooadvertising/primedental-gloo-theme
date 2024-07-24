cf94b03a715f504683791433acd0f8aa1d015715
Pablo Sazo <pablo@glooadvertising.com.au>
2024-07-24 12:00:12 +1000

    close mobile menu

--- template-parts/mobile-menu.php
+++ template-parts/mobile-menu.php
@@ -1,11 +1,13 @@
 <div id="mobile-menu" class="menu-visible menu-hidden flex items-center justify-center bg-lavender/90 text-white z-10 rounded-xl lg:hidden">
+   <div id="closeMobileMenu" class="absolute top-4 right-4 cursor-pointer flex items-center justify-center gap-4 aspect-square w-16 bg-light-lavender/50 rounded-full opacity-50 hover:opacity-100 transition-all">
+        <img src="<?php echo get_template_directory_uri(); ?>/resources/images/arrow-back.png" alt="Close">
+   </div>
     <nav class="mobile-menu">
         <ul class="grid uppercase 2xl">
             <li><a href="<?php echo site_url('/'); ?>" class="<?php echo $slug == '' ? '!font-semibold' : null; ?>">Home</a></li>
             <li><a href="<?php echo site_url('/about'); ?>" class="<?php echo $slug == 'about' ? '!font-semibold' : null; ?>">About</a></li>
             <li><a href="<?php echo site_url('/services'); ?>" class="<?php echo $slug == 'services' ? '!font-semibold' : null; ?>">Services</a></li>
             <li><a href="<?php echo site_url('/contact'); ?>" class="<?php echo $slug == 'contact' ? '!font-semibold' : null; ?>">Contact</a></li>
-            <li><a href="" id="closeMobileMenu">Close</a></li>
         </ul>
     </nav>
     <?php get_template_part('template-parts/book-online'); ?>
