1323874f18d754bd2ebed6212afae779a0c79f5e
Pablo Sazo <pablo@glooadvertising.com.au>
2024-07-24 11:46:37 +1000

    welcome break

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
 <div class="flex items-center justify-center h-full">
-	<div class="px-12 py-16 rounded-xl bg-light-lavender h-full w-full flex justify-start items-center relative">
+	<div class="px-6 sm:px-12 py-16 rounded-xl bg-light-lavender h-full w-full flex justify-start items-center relative">
 		<div class="flex w-full lg:w-lg xl:w-xl mx-auto 2xl:mx-12 3xl:mx-48">
-			<h1 class="text-5xl lg:text-7xl uppercase font-extralight text-lavender mb-6">Welcome to<br><?php bloginfo('name'); ?></h1>
+			<h1 class="text-5xl lg:text-7xl uppercase font-extralight text-lavender mb-6">Welcome to <br class="hidden sm:block"><?php bloginfo('name'); ?></h1>
 		</div>
 		<svg viewBox="0 0 307 468" fill="none" xmlns="http://www.w3.org/2000/svg" class="absolute bottom-0 right-0 h-[33%] md:h-[50%]">
 			<path d="M307 0.0950855V468H233.909C105.311 468 0 362.743 0 234C0 105.257 105.216 0 233.909 0H307V0.0950855Z" fill="#6E7791"/>
