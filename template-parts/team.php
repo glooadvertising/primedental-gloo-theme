8b6c001044ee71b58240662371144213c4b0355c
Pablo Sazo <pablo@glooadvertising.com.au>
2024-07-24 15:46:47 +1000

    profile modal

--- template-parts/team.php
+++ template-parts/team.php
@@ -25,8 +25,8 @@
                     <h1 class="font-bold uppercase mt-3 text-sm"><?php the_title(); ?></h1>
                     <p class="uppercase text-xs mb-3"><?php the_field('role')?></p>
                     <dialog id="<?php the_ID(); ?>" class="page-content px-4 md:px-12 pt-4 md:pt-12 p-24 mt-4 lg:m-auto m-auto rounded-lg max-w-['calc(100% - 4rem)'] md:max-w-2xl !max-h-full bg-lavender text-white backdrop:bg-lavender/50"> 
-                        <span class="flex mb-6 gap-6 items-end">
-                            <img src="<?php echo get_the_post_thumbnail() ? get_the_post_thumbnail_url() : get_template_directory_uri() . '/resources/images/avatar.jpg'; ?>" alt="" class="w-32 rounded-md aspect-square <?php echo get_the_post_thumbnail() ? null : 'mix-blend-multiply'; ?>">
+                        <span class="grid md:flex mb-6 gap-6 items-end">
+                            <img src="<?php echo get_the_post_thumbnail() ? get_the_post_thumbnail_url() : get_template_directory_uri() . '/resources/images/avatar.jpg'; ?>" alt="" class="w-full md:w-32 rounded-md aspect-square <?php echo get_the_post_thumbnail() ? null : 'mix-blend-multiply'; ?>">
                             <span>
                             <h1 class="font-bold uppercase mt-3 text-md text-light-lavender"><?php the_title(); ?></h1>
                             <p class="uppercase text-xs"><?php the_field('role')?></p>
